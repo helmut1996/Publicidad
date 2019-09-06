eef377ed60f365e2b52a624ed6adbea80504a247
helmut1996 <helmut-1521@hotmail.com>
2019-09-06 08:46:59 +0200

    Galeria y comentarios

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
-  });
+  })
 
   //paralax
   document.addEventListener('DOMContentLoaded', function() {
@@ -10,15 +10,20 @@
     var instances = M.Parallax.init(elems);
   });
 
-  //
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems);
-  });
 
   //modal
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
+  });
+  //imagenes
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
+  });
+
+  //iconos
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
   });
